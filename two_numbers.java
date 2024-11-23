8b9f716ff73dcba6ca87e4b03f95f29ed0dca1b2
Syed Athuallah R <syedathaullah786786@gmail.com>
2024-11-23 17:13:48 +0530

    Update two_numbers.java

--- two_numbers.java
+++ two_numbers.java
@@ -8,7 +8,7 @@ public class two_numbers {
 }
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       HashMap<Integer,Integer> map = new HashMap();
+       HashMap<Integer,Integer> map = new HashMap<>();
        for(int i = 0; i<nums.length;i++){
         int complement = target - nums[i];
         if(map.containsKey(complement)){
