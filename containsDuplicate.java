28908b937a9092e2a15944b1ca58b4cd05e46f13
Syed Athuallah R <syedathaullah786786@gmail.com>
2024-11-28 22:34:23 +0530

    217. Contain Duplicate

---
+++ containsDuplicate.java
@@ -0,0 +1,20 @@
+// The TIme Complexity:
+// Best Case and Average Case: O(1) as we are using the hashset which takes one unit of time
+
+import java.util.HashSet;
+
+public class containsDuplicate { 
+    public boolean contains_Duplicate(int[] nums){// This Approach runs upto 6 - 8ms
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : nums){
+            if(!set.add(i)) return true;
+        }return false;
+        
+    }
+   
+    public static void main(String[] args) {
+        containsDuplicate a = new containsDuplicate();
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(a.contains_Duplicate(nums));
+    }
+}
