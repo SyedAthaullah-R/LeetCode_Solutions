e64ea25a1b0cda57840a2a881234f73df1f58caa
Syed Athuallah R <syedathaullah786786@gmail.com>
2024-12-12 17:40:29 +0530

    152. Maximum Product Sub Array

---
+++ maximumProductSubArray.java
@@ -0,0 +1,19 @@
+// Time Complexity: O(n)
+
+public class maximumProductSubArray {
+    public int maxmimum_Product_SubArray(int[] nums){
+        int prefix = 1, suffix = 1, max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 1) suffix = 1;
+            prefix *= nums[i];
+            suffix *= nums[nums.length - i - 1];
+            max = Math.max(max, Math.max(prefix, suffix));
+        }return max;
+    }
+    public static void main(String[] args) {
+        maximumProductSubArray a = new maximumProductSubArray();
+        int[] nums = {2, 3, -1, 4};
+        System.out.println(a.maxmimum_Product_SubArray(nums));
+    }
+}
