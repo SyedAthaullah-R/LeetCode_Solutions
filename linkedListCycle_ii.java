76fc542d4892c92010537d9f9f735e0e05068865
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-02-05 21:41:00 +0530

    142. Linked List Cycle ii

--- linkedListCycle_ii.java
+++ linkedListCycle_ii.java
@@ -1,8 +1,38 @@
+// Time Complexity: O(n)
+
 class ListNode{
     int val;
-    
-
+    ListNode next;
+    ListNode(int val){
+        this.val = val;
+        this.next = null;
+    }
 }
 public class linkedListCycle_ii {
-    
+    public ListNode linked_List_Cycle_ii(ListNode head){
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                fast = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        linkedListCycle_ii a = new linkedListCycle_ii();
+        System.out.println(a.linked_List_Cycle_ii(head));
+        
+    }
 }
