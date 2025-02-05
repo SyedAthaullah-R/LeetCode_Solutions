84a433cf280b3b42d878e53ae55adc39b37bd444
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-02-05 20:42:03 +0530

    141. Linked List Cycle

---
+++ linkedListCycle.java
@@ -0,0 +1,31 @@
+// Time COmplexity: O(n)
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        val = this.val;
+        next = null;
+    }
+}
+public class linkedListCycle {
+    public boolean linked_List_Cycle(ListNode head){
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        } return false;
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        linkedListCycle a = new linkedListCycle();
+        System.out.println(a.linked_List_Cycle(head));
+        
+
+    }
+}
