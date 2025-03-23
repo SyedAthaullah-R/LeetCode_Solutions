e92c119c90f05c085d8fa98cc03ce003883a61a3
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-03-23 20:57:14 +0530

    143. Reorder List

---
+++ reorderList.java
@@ -0,0 +1,27 @@
+// Time Complexity: O(n)
+public class reorderList {
+    public void re_Order_List(ListNode head){
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode secondHalf = slow.next;
+        slow.next = null;
+        ListNode prev = null, curr = secondHalf;
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        ListNode first = head, second = prev;
+        ListNode temp1 = first.next, temp2 = second.next;
+        while(second != null){
+            first.next = second;
+            second.next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
