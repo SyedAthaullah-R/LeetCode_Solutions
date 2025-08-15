fa43d106024fc10a2cf6326c341bb7e489515e4b
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-08-15 12:07:03 +0530

    73. Set Matrix Zeroes

---
+++ setMatrixZeroes.java
@@ -0,0 +1,40 @@
+// Time Complexity: O(m + n)
+import java.util.Arrays;
+
+public class setMatrixZeroes {
+    public void Set_Zeroes(int[][] matrix){
+        boolean firstRow = false, firstCol = false;
+        int m = matrix.length, n = matrix[0].length;
+        for(int j = 0; j < n; j++)
+            if(matrix[0][j] == 0) firstRow = true;
+        for(int i = 0; i < m; i++)
+            if(matrix[i][0] == 0) firstCol = true;
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+        if(firstRow){
+            for(int j = 0; j < n; j++)
+                matrix[0][j] = 0;
+        }
+        if(firstCol){
+            for(int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+    }
+    public static void main(String[] args){
+        setMatrixZeroes a = new setMatrixZeroes();
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        a.Set_Zeroes(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
