c8e23974f5870fb07feb11289bce744961f91da7
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-12-05 19:26:54 +0530

    3432. Count Partitions with Even Sum Difference

---
+++ countPartitionsWithEvenSumDifference.java
@@ -0,0 +1,14 @@
+public class countPartitionsWithEvenSumDifference {
+    public int coutnPartitions(int[] nums){
+        int sum = 0;
+        for(int i : nums)
+            sum += i;
+        return sum % 2 == 0 ? nums.length - 1 : 0;
+    }
+    public static void main(String[] args){
+        countPartitionsWithEvenSumDifference a = new countPartitionsWithEvenSumDifference();
+        int[] nums = {10, 10, 3, 7, 6};
+        System.out.println(a.coutnPartitions(nums));
+
+    }
+}
