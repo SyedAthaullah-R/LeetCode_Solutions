ccba546ab5c69b15fffd7e053a1c1f9f0901a35c
Syed Athuallah R <syedathaullah786786@gmail.com>
2026-04-30 20:01:46 +0530

    101. Symmetric Tree

---
+++ symmetricTree.java
@@ -0,0 +1,18 @@
+// Time Complexity: O(n)
+public class symmetricTree {
+    public boolean check(TreeNode l, TreeNode r) {
+        if (l == null && r == null)
+            return true;
+        if (l == null || r == null)
+            return false;
+        if (l.val == r.val && check(l.left, r.right) && check(l.right, r.left))
+            return true;
+        return false;
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return check(root.left, root.right);
+    }
+}
